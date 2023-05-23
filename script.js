b4dcfe857ede385b6da67ab0f14aeedd8382267a
KaiqueFj <kaiquelferraz@hotmail.com>
2023-05-23 03:54:19 -0300

    fix: fixed bug when the user insert a operation and the old values would appear.

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let parentElement = document.querySelector(".buttons");
 let calcScreen = document.querySelector(".screen");
-let numbers = [];
+let result;
 let calcNumbers;
+let numbers = [];
 
 const Calculate = function () {
   parentElement.addEventListener("click", function (e) {
     e.preventDefault();
     const target = e.target.closest(".calcBtn");
     if (!target) return;
-    console.log(target.textContent);
 
-    if (target.textContent === "C") {
+    if (target.textContent === "=") {
+      calcScreen.textContent = eval(calcNumbers);
+      numbers = [];
+      numbers.push(calcScreen.textContent);
+    } else if (target.textContent === "C") {
       numbers = [];
       calcScreen.textContent = "0";
-    } else if (target.textContent === "=") {
-      calcScreen.textContent = eval(calcNumbers);
     } else {
       numbers.push(target.textContent);
       calcNumbers = numbers.join("");
-
       calcScreen.textContent = calcNumbers;
     }
   });
 };
-
 
 Calculate();
